01473d4e640d4ad161159ddc2857591378f97483
GINK03 <angeldust03@gmail.com>
2013-07-05 14:35:56 -0400

    nginx files

---
+++ nginx/gmain.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+extern "C"{
+#include<string.h>
+#include<stdint.h>
+#include"ngx_config.h"
+#include<ngx_core.h>
+#include<ngx_http.h>
+};
+
+using namespace std;
+int32_t main(){
+   cout << "hage" << endl;
+};
---
+++ nginx/mk.sh
@@ -0,0 +1,1 @@
+g++-4.8 -std=c++11 gmain.cpp -o a.out -I/home/juiz/nginx/nginx/src/core -I/home/juiz/nginx/nginx/src/os/unix -I/home/juiz/nginx/nginx/src/http -I/home/juiz/nginx/nginx/src/event
