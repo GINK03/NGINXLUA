4fa5418feba34cc616eaf22131b2be3e1587d992
GINK03 <angeldust03@gmail.com>
2013-07-13 17:06:17 -0400

    BerkleyDB source code

--- modules/Mk.sh
+++ modules/Mk.sh
@@ -1,1 +1,2 @@
 g++-4.8 -std=c++11 -fPIC -shared -fpermissive Template.cpp -o libmodule.so
+g++-4.8 -std=c++11 -fPIC -fpermissive Template.cpp Berkley.cpp -I/usr/include -L/usr/lib/i386-linux-gnu/ -ldb_cxx-5.1 -o a.o
