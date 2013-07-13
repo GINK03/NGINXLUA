2f0a4062991548f83f2761c2c9d1b8618a9f24d4
GINK03 <angeldust03@gmail.com>
2013-07-13 13:28:49 -0400

    add oracle berkleydb

--- modules/Template.cpp
+++ modules/Template.cpp
@@ -1,24 +1,56 @@
 #include<string>
 #include<sstream>
-//** プロトタイプ宣言
+#include<vector>
+#include<boost/lexical_cast.hpp>
+#include<iostream>
 extern "C"{
 #include"hiredis/hiredis.h"
-const char* cxxBridge(char* in);
+};
+//** プロトタイプ宣言
+typedef struct{
+  size_t len;
+  u_char *data;
+} ngx_str_t;
+
+extern "C"{
+const char* cxxBridge(ngx_str_t in);
 };
 
 namespace C{
+
+using str = std::string;
+using sstr  = std::stringstream;
+using boost::lexical_cast;
+static auto funcPool = [](int32_t i){
+  static int32_t k = 0;
+  static std::vector<std::string> vs = {"mem1", "mem2"};
+  auto regist = {'c', ':', char(k + i + 48)};
+  vs.push_back(regist);
+  ++k;
+  return vs[k];
+};
+
 class HTML{
   private:
     HTML(){};
   public:
-    static std::string Header(){
-      std::stringstream ss;
-      ss << "<html><head></head><body>Wellcome to C++11 world</body></html>";
+    static str Header(unsigned char* in){
+      str input("");
+      if(in != nullptr){
+        input = lexical_cast<str>(in);
+      };
+      sstr ss;
+      ss << "<html><head></head><body>Wellcome to C++11 world</br>" 
+         << funcPool(1) 
+         << "</br> it's too hot</br>"
+         << input
+         << "</body></html>";
+      std::cout << "[INPUT]" << input << std::endl;
       return ss.str();
     };
 };
 };//namespace
 
-const char* cxxBridge(char* in){
-  return C::HTML::Header().c_str();
+const char* cxxBridge(ngx_str_t in){
+  return C::HTML::Header(in.data).c_str();
 };
--- nginx/objs/ngx_auto_config.h
+++ nginx/objs/ngx_auto_config.h
@@ -1,7 +1,7 @@
 #define NGX_CONFIGURE " --with-cc=gcc --with-ld-opt='-L/home/juiz/NGINXLUA/modules -lmodule -Wl,-rpath,/home/juiz/NGINXLUA/modules' --with-cc-opt='-I /usr/local/include' --with-pcre --add-module=ngx_http_bridge"
 
 #ifndef NGX_COMPILER
-#define NGX_COMPILER  "gcc 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
+#define NGX_COMPILER  "gcc 4.6.4 (Ubuntu/Linaro 4.6.4-1ubuntu1~12.04) "
 #endif
 
 
