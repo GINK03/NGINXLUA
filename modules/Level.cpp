188bb6107fef0b4e501e12d6017f3372dc822b35
GINK03 <angeldust03@gmail.com>
2013-07-18 12:19:05 -0400

    modefied LevelDB

--- modules/Level.cpp
+++ modules/Level.cpp
@@ -3,8 +3,18 @@ extern "C"{
 };
 #include<iostream>
 #include<string>
+#include<memory>
 
-using namespace std;
+namespace C{
+using std::string;
+class Dispose{
+  public:
+    Dispose(){};
+};
+class Unlock{
+  public:
+    Unlock(){};
+};
 class Level{
   private:
     leveldb_t* _db;
@@ -14,38 +24,56 @@ class Level{
     char*   _err;
     char*   _read;
     size_t  _read_len;
+    string _dbname;
   public:
-    Level(){
-      _options = leveldb_options_create();
+    Level(const string& dbname)
+    :_options(leveldb_options_create()),
+    _woptions(leveldb_writeoptions_create()),
+    _roptions(leveldb_readoptions_create()),
+    _dbname(dbname)
+    {
       leveldb_options_set_create_if_missing(_options, 1);
-      _db = leveldb_open(_options, "testdb.ldb", &_err);
-      if(_err != nullptr){cerr << "open fail:" << _err << endl; /*return 0;*/};
-      //leveldb_free((void*)err); err = nullptr;
+      _db = leveldb_open(_options, _dbname.c_str(), &_err);
+      if(_err != nullptr){/*cerr << "open fail:" << _err << endl;*/};
     };
-    bool put(){
-      _woptions = leveldb_writeoptions_create();
-      leveldb_put(_db, _woptions, "key", 3, "value", 5, &_err);
-      if(_err != nullptr){cerr << "write fail:" << _err << endl; return false;};
+    bool put(const string& key, const string& val){
+      leveldb_put(_db, _woptions, key.c_str(), key.length(), val.c_str(), val.length(), &_err);
+      if(_err != nullptr){/*cerr << "write fail:" << _err << endl;*/ return false;};
       return true;
-      //leveldb_free((void*)err); err = nullptr;
     };
-    bool read(){
-      _roptions = leveldb_readoptions_create();
-      _read = leveldb_get(_db, _roptions, "key", 3, &_read_len, &_err);
-      if(_err != nullptr){cerr << "get fail:" << _err << endl; return false;};
-      cout << "read:" << string(_read, _read + _read_len) << " readlen:" << _read_len << endl;
-      return true;
-      //leveldb_free((void*)err); err = nullptr;
+    string read(const string& key){
+      _read = leveldb_get(_db, _roptions, key.c_str(), key.length(), &_read_len, &_err);
+      if(_err != nullptr){/*cerr << "get fail:" << _err << endl;*/ return "";};
+      return string(_read, _read + _read_len);
     };
     ~Level(){
+      //not link 
+      //leveldb_close(_db);
+    };
+    Level& operator~(){
+      std::cerr << "a little chilly 1 " << std::endl;
+      return *this;
+    };
+    operator Unlock(){
+      std::cerr << "a little chilly 2 " << std::endl;
       leveldb_close(_db);
-      leveldb_destroy_db(_options, "testdb", &_err);
-      if(_err != nullptr){cerr << "destroy fail:" << _err << endl; /*return 0;*/};
+    };
+    /* 特殊機能のDBのデストロイ */
+    /* DBのクローズが発生して、ハンドルポイントが失われている必要がある */
+    template<class LEVEL>
+    static bool Dispose(LEVEL level){
+      leveldb_destroy_db(level->_options, level->_dbname.c_str(), &(level->_err));
+      if(level->_err != nullptr){std::cerr << "destroy fail:" << level->_err << std::endl;};
     };
 };
+};//namespace C
 int main(){
-  Level lev();
-  cout << "test " << endl;
+  std::shared_ptr<C::Level> level(new C::Level("leveldb.ldb"));
+  //level->put("atsui", "nemui");
+  std::string res = level->read("atsui");
+  C::Level::Dispose(level);
+  C::Unlock(~(*level));
+  std::cout << "leveldbtest: " << res << std::endl;
 };
 
 
